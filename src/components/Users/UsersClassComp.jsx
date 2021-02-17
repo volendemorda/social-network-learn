3ca801ec4e8ff7d95c7218c1f2b3886be4037ad9
robert kuzhin <rk.ku.202014@gmail.com>
2021-02-17 13:24:31 +0300

    test

--- src/components/Users/UsersClassComp.jsx
+++ src/components/Users/UsersClassComp.jsx
@@ -1,32 +1,31 @@
-import React from 'react'
-import '../Users/users.css'
+import React from "react";
+import "../Users/users.css";
 import UserAPI from "./UsersAPI";
 import Proloader from "./proloader/proloader";
 
-
 class Users extends React.Component {
-     componentDidMount(){
-         this.props.getUsersThunkCreator(this.props.CurrentPage, this.props.pageSize)
-     }
-    onChangedPage = (item) => {
-        this.props.pageUsersActionCreator(item)
-        this.props.getUsersThunkCreator(item, this.props.pageSize)
-    }
-    render() {
-        return (
-            <div>
-                {this.props.isFetching === true
-                ? <Proloader/>
-                : null
-                }
-                <UserAPI
-                    {...this.props}
-                    onChangedPage={this.onChangedPage}
-                    isGetDataFollow={this.props.isGetDataFollow}
-                />
-            </div>
-        )
-    }
+  componentDidMount() {
+    this.props.getUsersThunkCreator(
+      this.props.CurrentPage,
+      this.props.pageSize
+    );
+  }
+  onChangedPage = (item) => {
+    this.props.pageUsersActionCreator(item);
+    this.props.getUsersThunkCreator(item, this.props.pageSize);
+  };
+  render() {
+    return (
+      <div>
+        {this.props.isFetching === true ? <Proloader /> : null}
+        <UserAPI
+          {...this.props}
+          onChangedPage={this.onChangedPage}
+          isGetDataFollow={this.props.isGetDataFollow}
+        />
+      </div>
+    );
+  }
 }
 
-export default Users
+export default Users;
